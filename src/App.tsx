e7c4c48eb39644752e40b71fe143c93b9182f50a
Rodrigo <sistsayao@gmail.com>
2026-07-16 18:21:42 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ class App extends Component<{}, State> {
       <div className="App">
         <p className="App__message">
           {this.state.pressedKey
-            ? `The last pressed key is [${this.state.pressedKey}]`
+            ? `The last pressed key is ${this.state.pressedKey}`
             : 'Nothing was pressed yet'}
         </p>
       </div>
