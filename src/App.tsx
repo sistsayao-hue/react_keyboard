76f475445305ab3180f6b53d586d3224380be03f
Rodrigo <sistsayao@gmail.com>
2026-07-18 23:38:09 -0300

    Change displayed pressed key format in App component

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ class App extends Component<{}, State> {
       <div className="App">
         <p className="App__message">
           {this.state.pressedKey
-            ? `The last pressed key is ${this.state.pressedKey}`
+            ? `The last pressed key is [${this.state.pressedKey}]`
             : 'Nothing was pressed yet'}
         </p>
       </div>
